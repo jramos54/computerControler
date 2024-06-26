2cf4b16fb5f0d247209f3be9a76f8f762396ddc0
Joel Ramos Molina <joelramosmolina@MacBook-Pro-de-superuser128.local>
2024-06-26 14:42:32 -0600

    mac

---
+++ codes/screen.py
@@ -0,0 +1,178 @@
+import pygetwindow as gw
+import pyautogui
+import time
+
+def work_time(seconds):
+    hours=seconds//3600
+    minutes=(seconds%3600)//60
+    second=seconds%60
+    return f"{int(hours):02}:{int(minutes):02}:{second:.0f}"
+    
+
+def windows_navigate():
+    
+    pyautogui.keyDown('alt')
+    time.sleep(1)
+    windows=gw.getAllTitles()
+    for window in windows:
+        pyautogui.press('tab')
+        time.sleep(1)
+    pyautogui.keyUp('alt')
+    
+    
+def window_select(title):
+    
+    for window in gw.getWindowsWithTitle(title):
+        if title.lower() in window.title.lower():
+            window.activate()
+            time.sleep(1)
+            window.maximize()
+            break
+    
+
+def postman_work():
+    
+    pyautogui.keyDown('ctrl')
+    time.sleep(1)
+    pyautogui.press('tab')
+    time.sleep(1)
+    pyautogui.keyUp('ctrl')
+    time.sleep(5)
+    for i in range(7):
+        mouse_movement()
+    
+    pyautogui.keyDown('ctrl')
+    time.sleep(1)
+    pyautogui.press('enter')
+    time.sleep(1)
+    pyautogui.keyUp('ctrl')
+    time.sleep(15)
+    for i in range(7):
+        mouse_movement()
+ 
+    
+def mouse_movement():
+    width_screen, heigth_screen = pyautogui.size()
+    active_window = pyautogui.getActiveWindow()
+    
+    if active_window:
+        center_x = active_window.left + active_window.width // 2
+        center_y = active_window.top + active_window.height // 2
+
+    pyautogui.moveTo(center_x, center_y,duration=2)
+    time.sleep(2)
+    
+    point1=(center_x-width_screen//3,center_y+heigth_screen//3)
+    point2=(center_x+1*(width_screen//3),center_y+heigth_screen//3)
+    point3=(center_x-1*(width_screen//3),center_y-1*(heigth_screen//3))
+    point4=(center_x+1*(width_screen//3),center_y-1*(heigth_screen//3))
+    
+    pyautogui.moveTo(point1[0], point1[1],duration=2)
+    time.sleep(2)
+    pyautogui.moveTo(point2[0], point2[1],duration=2)
+    time.sleep(2)
+    pyautogui.moveTo(point3[0], point3[1],duration=2)
+    time.sleep(2)
+    pyautogui.moveTo(point4[0], point4[1],duration=2)
+    time.sleep(2)
+ 
+
+def write_vscode():
+    pyautogui.hotkey('ctrl', 'n')
+    time.sleep(2)  
+
+    input_path="codigo.txt"
+    with open(input_path, 'r') as archivo:
+        codigo = archivo.readlines()
+    
+    for line in codigo:
+        line=line.replace('\t',' '*4)
+        if line != '\n':
+            if line.endswith('\n'):
+                line=line[:-1]
+        else:
+            line.replace('\n','')
+        pyautogui.press('home')
+        pyautogui.write(line, interval=0.5)
+        pyautogui.press('esc')
+        pyautogui.press('enter')
+        
+
+def chrome_work():
+    pyautogui.keyDown('ctrl')
+    time.sleep(1)
+    pyautogui.press('tab')
+    time.sleep(1)
+    pyautogui.keyUp('ctrl')
+    time.sleep(3)
+    for i in range(6):
+        mouse_movement()
+
+
+def outlook_work():
+    time.sleep(3)
+    for i in range(6):
+        mouse_movement()
+        
+        
+def teams_work():
+    time.sleep(3)
+    for i in range(6):
+        mouse_movement()
+
+
+def excel_work():
+    time.sleep(3)
+    for i in range(9):
+        mouse_movement()
+
+
+def power_shell():
+    window_select('PowerShell')
+    pyautogui.keyDown('alt')
+    time.sleep(1)
+    pyautogui.press('F4')
+    time.sleep(1)
+    pyautogui.keyUp('alt')
+
+
+def inspector_work():
+    time.sleep(3)
+    for i in range(3):
+        mouse_movement()
+
+
+def source_tree():
+    time.sleep(3)
+    for i in range(2):
+        mouse_movement()
+
+if __name__ == "__main__":
+    pyautogui.PAUSE = 0.5
+    pyautogui.FAILSAFE = False
+    
+    start_time=time.time()
+    
+    for i in range(50):
+        
+        window_select('Excel')
+        excel_work()
+        window_select('Google Chrome')
+        chrome_work()
+        window_select('DevTools')
+        inspector_work()
+        window_select('EZASSI')
+        postman_work()
+        window_select('Outlook')
+        outlook_work()
+        window_select('Teams')
+        teams_work()
+        window_select('Sourcetree')
+        source_tree()
+        
+
+    end_time=time.time()
+    
+    print(work_time(end_time-start_time))
+    
+    power_shell()
---
+++ macOS.py
@@ -0,0 +1,149 @@
+import pyautogui, keyboard
+import subprocess
+import time, os, random
+
+def work_time(seconds):
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    second = seconds % 60
+    return f"{int(hours):02}:{int(minutes):02}:{second:.0f}"
+
+def window_select(title):
+    print(f"Selecting {title} window")
+    cmd = f'osascript -e \'activate application "{title}"\''
+    subprocess.call(cmd, shell=True)
+
+def postman_work():
+    window_select("Postman")
+    # time.sleep(60)
+
+    for i in range(10):
+        pyautogui.keyDown('command')
+        pyautogui.keyDown('shift')
+        pyautogui.press('{')
+        pyautogui.keyUp('command')
+        pyautogui.keyUp('shift')
+        # keyboard.press_and_release("shift+command,42")
+        time.sleep(60)
+
+def mouse_movement():
+    x=[125,375,750,1000]
+    y=[250,500,750,1000]
+    width,length=pyautogui.size()
+    xpos=random.choice(x)
+    ypos=random.choice(y)
+    pyautogui.moveTo(xpos,ypos)
+    time.sleep(60)
+
+
+def chrome_work():
+    window_select("Google Chrome")
+    pyautogui.keyDown('ctrl')
+    time.sleep(1)
+    pyautogui.press('tab')
+    time.sleep(1)
+    pyautogui.keyUp('ctrl')
+    time.sleep(3)
+    for i in range(10):
+        mouse_movement()
+
+def visual_work():
+    # keys=["(",'"',"'","{","["]
+    window_select("Visual Studio Code")
+    pyautogui.keyDown("command")
+    pyautogui.press("n")
+    pyautogui.keyUp("command")
+
+    time.sleep(5)
+
+    directorio="codes"
+    archivos=os.listdir(directorio)
+    archivo=random.choice(archivos)
+
+    ruta=os.path.join(directorio,archivo)
+
+    with open(ruta,'r',encoding='utf-8') as file:
+        contenido=file.read()
+
+    lines=contenido.split('\n')
+
+    for line in lines:
+        # pyautogui.hotkey('command', 'left')
+        
+        line.replace("\t"," "*4)
+
+        if line != '\n':
+            if line.endswith('\n'):
+                line=line[:-1]
+        else:
+            line.replace('\n','')
+
+        keyboard.press_and_release('command + left')
+        time.sleep(2)
+        for _ in line:
+            keyboard.write(_)
+            time.sleep(.25)
+        keyboard.press_and_release('enter')
+        keyboard.press_and_release('command + left')
+        time.sleep(5)
+
+def terminal_work():
+    window_select("Terminal")
+    pyautogui.keyDown('ctrl')
+    time.sleep(1)
+    pyautogui.press('tab')
+    time.sleep(1)
+    pyautogui.keyUp('ctrl')
+    time.sleep(3)
+    for i in range(10):
+        mouse_movement()
+
+def docker_work():
+    window_select("Docker Desktop")
+    for i in range(10):
+        mouse_movement()
+        time.sleep(60)
+
+def finder_work():
+    window_select("Finder")
+    for i in range(5):
+        mouse_movement()
+        time.sleep(60)
+
+
+if __name__ == "__main__":
+
+    time.sleep(30)
+    pyautogui.PAUSE = 0.5
+    pyautogui.FAILSAFE = False
+    
+    start_time = time.time()
+    for i in range(1):
+        print(f"Iteracion {i}")
+        chrome_work()
+        time.sleep(60)
+        postman_work()
+        time.sleep(60)
+        finder_work()
+        time.sleep(60)
+        visual_work()
+        time.sleep(60)
+        terminal_work()
+        time.sleep(60)
+        docker_work()
+        time.sleep(60)
+        chrome_work()
+        time.sleep(60)
+        # # # visual_work()
+        postman_work()
+        time.sleep(60)
+        terminal_work()
+        time.sleep(60)
+        finder_work()
+        time.sleep(60)
+        postman_work()
+
+    end_time = time.time()
+    
+    print(work_time(end_time - start_time))
+            
